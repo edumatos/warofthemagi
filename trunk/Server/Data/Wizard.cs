56515bf142998df6840a885e4f756678b448f8f5
derek.a.bliss@gmail.com <derek.a.bliss@gmail.com@5b8fbb62-3593-d25f-ef98-e17e702b9ba1>
2011-01-23 05:37:24 +0000

    base classes and skeleton code for updating the server

--- trunk/Server/Data/Wizard.cs
+++ trunk/Server/Data/Wizard.cs
@@ -1,18 +1,70 @@
 ﻿using PlayerIO.GameLibrary;
+using System.Collections.Generic;
 
 namespace WotMServer.Data
 {
-    class Wizard : BasePlayer
+    public class Wizard : BasePlayer
     {
+        public enum ElementType
+        {
+            NONE                = 0,
+            FIRE                = 1,
+            EARTH               = 2,
+            WIND                = 3,
+            WATER               = 4,
+            LIGHTNING           = 5,
+            CAPTAIN_PLANET      = 6
+        }
+
         #region Properties
 
-        public int Wins { get; set; }
+        public bool Ready { get; set; }
 
-        public int Losses { get; set; }
+        public int ViewPlayerId { get; set; }
 
         public int Mana { get; set; }
 
-        // insert other things here
+        public ElementType Element { get; set; }
+
+        public List<BaseBuilding> Buildings { get; set; }
+
+        public List<BaseUnit> Units { get; set; }
+
+        public List<BaseUnit> Neutrals { get; set; }
+
+        public List<Spell> Spells { get; set; }
+
+        #endregion
+
+        #region Constructors
+
+        public Wizard()
+        {
+            Buildings = new List<BaseBuilding>();
+            Units = new List<BaseUnit>();
+            Neutrals = new List<BaseUnit>();
+            Spells = new List<Spell>();
+            Element = ElementType.NONE;
+        }
+
+        #endregion
+
+        #region Methods
+
+        /// <summary>
+        /// Update wizard's objects.
+        /// </summary>
+        /// <param name="delta">time elasped since last update</param>
+        public void Update(float delta)
+        {
+            // update all the objects
+            foreach (BaseBuilding b in Buildings)
+                b.Update(delta);
+            foreach (BaseUnit bu in Units)
+                bu.Update(delta);
+            foreach (BaseUnit bu in Neutrals)
+                bu.Update(delta);
+        }
 
         #endregion
     }
--- trunk/Server/WotMLobby.cs
+++ trunk/Server/WotMLobby.cs
@@ -5,18 +5,57 @@ using WotMServer.Data;
 namespace WotMServer
 {
     [RoomType("WotMLobby")]
-    class WotMLobby : Game<Wizard>
+    public class WotMLobby : Game<Wizard>
     {
         public override void GameStarted()
         {
-            Console.Write("Lobby started");
+            Console.WriteLine("Lobby started");
             base.GameStarted();
         }
 
         public override void GameClosed()
         {
-            Console.Write("Lobby closed");
+            Console.WriteLine("Lobby closed");
             base.GameClosed();
+        }
+
+        public override void UserJoined(Wizard player)
+        {
+            Message m = Message.Create("WizardList");
+
+            m.Add(player.Id);
+
+            foreach (Wizard w in Players)
+                m.Add(w.Id);
+
+            player.Send(m);
+
+            Message b = Message.Create("WizardJoined");
+            b.Add(player.Id);
+            Broadcast(b);
+
+            Console.WriteLine("Wizard " + player.Id + " joined the lobby.");
+
+            base.UserJoined(player);
+        }
+
+        public override void UserLeft(Wizard player)
+        {
+            Message m = Message.Create("WizardLeft");
+            m.Add(player.Id);
+            Broadcast(m);
+            Console.WriteLine("Wizard " + player.Id + " left the lobby.");
+            base.UserLeft(player);
+        }
+
+        public override void GotMessage(Wizard player, Message message)
+        {
+            if (message.Type == "Text")
+            {
+                Console.WriteLine(player.ConnectUserId + ": " + message.GetString(0));
+                Broadcast("Text", player.ConnectUserId, message.GetString(0));
+            }
+            base.GotMessage(player, message);
         }
     }
 }
